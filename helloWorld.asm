aa08e2c14d58b3e5799b3ac7e80794e67f107f16
robinNcode <msmrobin518@gmail.com>
2021-01-16 01:03:54 +0600

    hello world

---
+++ helloWorld.asm
@@ -0,0 +1,28 @@
+.model large
+.stack 100h
+.code
+main proc
+    
+    mov ah,2
+    mov dl,'h'
+    int 21h
+    mov dl,'e'
+    int 21h
+    mov dl,'l'
+    int 21h
+    mov dl,'l'
+    int 21h
+    mov dl,'o'
+    int 21h    
+    mov dl,009
+    int 21h
+    mov dl,'W'
+    int 21h
+    mov dl,'o'
+    int 21h
+    mov dl,'r'
+    int 21h
+    mov dl,'l'
+    int 21h
+    mov dl,'d'
+    int 21h
