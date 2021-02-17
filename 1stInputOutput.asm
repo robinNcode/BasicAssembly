222c359b8a471950d5d637559f3264db97aaaed1
robinNcode <msmrobin518@gmail.com>
2021-02-18 03:45:55 +0600

    1st input output

---
+++ 1stInputOutput.asm
@@ -0,0 +1,32 @@
+.model smal
+.stack 100h
+.code
+
+main proc
+    
+    ;getting 1st input
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    ;getting 2nd input
+    mov ah,1
+    int 21h
+    mov bh,al
+    
+    ;displaying 1st input
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    ;displaying 2nd input
+    mov ah,2
+    mov dl,bh
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end
+    
